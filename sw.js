091b4bf64c0277147ae0b2046df13324cf4b15ea
Koki Takahashi <hakatasiloving@gmail.com>
2018-08-02 01:27:05 +0900

    Update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('/_nuxt/workbox.3de3418b.js')
+importScripts('https://hakatashi.github.io/smart-cube-timer/workbox.3de3418b.js')
 
 const workboxSW = new self.WorkboxSW({
   "cacheId": "nuxt",
@@ -8,29 +8,29 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/_nuxt/app.ac15ea284f788764969d.js",
-    "revision": "a05971d291d58a421476784d8e1c5902"
+    "url": "https://hakatashi.github.io/smart-cube-timer/app.4ec10c41eb7043af0555.js",
+    "revision": "c7b23e2df92a060f14092f0c21ab1711"
   },
   {
-    "url": "/_nuxt/layouts_default.4146977e09a9df14ab4f.js",
+    "url": "https://hakatashi.github.io/smart-cube-timer/layouts_default.4146977e09a9df14ab4f.js",
     "revision": "658bd8e4ef3f28ad8bb7b30754a5ce16"
   },
   {
-    "url": "/_nuxt/manifest.271ff794bcb74fe5151d.js",
-    "revision": "14cb929d7b9cdef6251593b47403df6f"
+    "url": "https://hakatashi.github.io/smart-cube-timer/manifest.204603551a4a7daad304.js",
+    "revision": "e50057ccb5d661fbaadfbe1e6d3d3f71"
   },
   {
-    "url": "/_nuxt/pages_index.89491d912b9b72ac2063.js",
+    "url": "https://hakatashi.github.io/smart-cube-timer/pages_index.89491d912b9b72ac2063.js",
     "revision": "2074560436a87e718aa1a38fd85e57e9"
   },
   {
-    "url": "/_nuxt/vendor.82700a98d93f5cae2550.js",
+    "url": "https://hakatashi.github.io/smart-cube-timer/vendor.82700a98d93f5cae2550.js",
     "revision": "cf1209de8fd593d3e6ef33dbbc2928aa"
   }
 ])
 
 
-workboxSW.router.registerRoute(new RegExp('/_nuxt/.*'), workboxSW.strategies.cacheFirst({}), 'GET')
+workboxSW.router.registerRoute(new RegExp('https://hakatashi.github.io/smart-cube-timer/.*'), workboxSW.strategies.cacheFirst({}), 'GET')
 
 workboxSW.router.registerRoute(new RegExp('/.*'), workboxSW.strategies.networkFirst({}), 'GET')
 
