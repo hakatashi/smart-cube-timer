c7efb1f527a4a8564cff2454194b854e7ad240e0
Koki Takahashi <hakatasiloving@gmail.com>
2018-08-06 19:28:33 +0900

    Implement solves history display

--- pages/solves.vue
+++ pages/solves.vue
@@ -1,8 +1,41 @@
 <template>
+	<v-container fluid grid-list-md>
+		<v-data-iterator
+			:items="solves"
+			:pagination.sync="pagination"
+			content-tag="v-layout"
+			row
+			wrap
+		>
+			<v-flex
+				slot="item"
+				slot-scope="props"
+				xs6
+				sm6
+				md4
+				lg3
+			>
+				<v-card>
+					<v-card-title><h4>{{(props.item.time / 1000).toFixed(2)}}</h4></v-card-title>
+				</v-card>
+			</v-flex>
+		</v-data-iterator>
+	</v-container>
 </template>
 
 <script>
+	import {getSolves} from '~/lib/db.js';
+
 	export default {
+		data() {
+			return {
+				solves: [],
+				pagination: {rowsPerPage: 10},
+			};
+		},
+		async mounted() {
+			this.solves = await getSolves();
+		},
 	}
 </script>
 
