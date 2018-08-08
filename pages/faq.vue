5226ae050a81320c15f3cc96ad5624b5822e2754
Koki Takahashi <hakatasiloving@gmail.com>
2018-08-09 03:32:14 +0900

    Add more description to faq page

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,12 +1,11 @@
 <template>
 	<v-container grid-list-lg reverse>
 		<div>
-			<h1>What is this?</h1>
-			<p>Smart Cube Timer</p>
-			<h1>What can I do with this?</h1>
+			<h3>What can I do with this?</h3>
 			<p><nuxt-link to="https://youtu.be/10ONTpO0mBM">https://youtu.be/10ONTpO0mBM</nuxt-link></p>
-			<h1>Is this useful?</h1>
-			<p>Yes</p>
+			<h3>What is "Inspection" and "Execution" on each stage?</h3>
+			<p>Inspection is the time you only thinking what alg to execute on the current cube. It is duration to the first move that is not U face turn or the move not canceled by the following moves.</p>
+			<p>Execution time is (total time) - (inspection time).</p>
 		</div>
 	</v-container>
 </template>
