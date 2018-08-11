2079a9e790ae1bf6e2c3bcdd3a01b9ed19d9a095
Koki Takahashi <hakatasiloving@gmail.com>
2018-08-11 20:09:44 +0900

    Display solve information as chips

--- pages/solves.vue
+++ pages/solves.vue
@@ -3,7 +3,7 @@
 		<v-layout row wrap>
 			<v-flex v-for="solve in solvesInfo" :key="solve.date" xs12>
 				<v-card class="solve">
-					<v-card-text class="pa-0 subheading text-xs-left">
+					<v-card-text class="pa-0 subheading text-xs-left solve-headline">
 						<strong>{{solve.timeText}}</strong>
 						<small class="solve-times">
 							<span
@@ -15,7 +15,24 @@
 							</span>
 						</small>
 					</v-card-text>
-					<v-card-text class="solve-date pa-0 text-xs-right">{{solve.dateText}}</v-card-text>
+					<v-layout class="solve-date pa-0">
+						<v-chip
+							v-for="info in solve.infos"
+							:key="info.id"
+							small
+							:color="info.color.startsWith('#') ? null : info.color"
+							:text-color="info.textColor.startsWith('#') ? null : info.textColor"
+							:style="{
+								backgroundColor: info.color.startsWith('#') ? info.color : '',
+								color: info.textColor.startsWith('#') ? info.textColor : '',
+							}"
+							class="solve-chip"
+						>
+							{{info.text}}
+						</v-chip>
+						<v-spacer></v-spacer>
+						<div>{{solve.dateText}}</div>
+					</v-layout>
 				</v-card>
 			</v-flex>
 		</v-layout>
@@ -25,7 +42,8 @@
 <script>
 	import {getSolves} from '~/lib/db.js';
 	import config from '~/lib/config.js';
-	import {formatTime, formatDate} from '~/lib/utils.js';
+	import {formatTime, formatDate, idealTextColor} from '~/lib/utils.js';
+	import {olls, plls} from '~/lib/data.js';
 
 	export default {
 		data() {
@@ -96,19 +114,62 @@
 		},
 		computed: {
 			solvesInfo() {
-				return this.solves.map((solve) => ({
-					...solve,
-					timeText: formatTime(solve.time),
-					dateText: formatDate(solve.date),
-					cross: formatTime(solve._crossTime),
-					f2l1: formatTime(solve._f2l1Time),
-					f2l2: formatTime(solve._f2l2Time),
-					f2l3: formatTime(solve._f2l3Time),
-					f2l4: formatTime(solve._f2l4Time),
-					oll: formatTime(solve._ollTime),
-					pll: formatTime(solve._pllTime),
-					auf: formatTime(solve._aufTime),
-				}))
+				return this.solves.map((solve) => {
+					const infos = [];
+
+					if (solve.isError) {
+						infos.push({
+							id: 'error',
+							text: 'Error',
+							color: 'orange darken-4',
+							textColor: 'white'
+						});
+					}
+
+					if (solve._isXcross) {
+						infos.push({
+							id: 'xcross',
+							text: 'XCross',
+							color: '#4A148C',
+							textColor: idealTextColor('#4A148C'),
+						});
+					}
+
+					if (solve._ollCase !== null) {
+						const [ollName] = olls[solve._ollCase];
+						infos.push({
+							id: 'ollcase',
+							text: ollName,
+							color: 'yellow lighten-2',
+							textColor: '',
+						});
+					}
+
+					if (solve._pllCase !== null) {
+						const [pllName] = plls[solve._pllCase];
+						infos.push({
+							id: 'pllcase',
+							text: pllName,
+							color: 'red lighten-1',
+							textColor: 'white',
+						});
+					}
+
+					return {
+						...solve,
+						infos,
+						timeText: formatTime(solve.time),
+						dateText: formatDate(solve.date),
+						cross: formatTime(solve._crossTime),
+						f2l1: formatTime(solve._f2l1Time),
+						f2l2: formatTime(solve._f2l2Time),
+						f2l3: formatTime(solve._f2l3Time),
+						f2l4: formatTime(solve._f2l4Time),
+						oll: formatTime(solve._ollTime),
+						pll: formatTime(solve._pllTime),
+						auf: formatTime(solve._aufTime),
+					};
+				})
 			},
 		},
 	}
@@ -117,6 +178,10 @@
 <style>
 	.solve {
 		padding: 0.3em 0.8em;
+	}
+
+	.solve-headline {
+		height: 22px;
 	}
 
 	.solve-times {
@@ -159,8 +224,19 @@
 		}
 	}
 
-
 	.solve-date {
 		font-size: 10px;
+		margin-bottom: 0 !important;
+	}
+
+	.solve-chip {
+		height: 14px;
+		font-size: 10px;
+		margin-top: 0;
+		margin-bottom: 0;
+
+		.v-chip__content {
+			padding: 0 6px;
+		}
 	}
 </style>
