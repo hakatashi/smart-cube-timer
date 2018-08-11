3723cc33f9e6313a2b9d0c1cbec4ea5291174db6
Koki Takahashi <hakatasiloving@gmail.com>
2018-08-12 00:27:56 +0900

    Fix link to youtube

--- pages/faq.vue
+++ pages/faq.vue
@@ -2,7 +2,7 @@
 	<v-container grid-list-lg reverse>
 		<div>
 			<h3>What can I do with this?</h3>
-			<p><nuxt-link to="https://youtu.be/10ONTpO0mBM">https://youtu.be/10ONTpO0mBM</nuxt-link></p>
+			<p><a href="https://youtu.be/10ONTpO0mBM">https://youtu.be/10ONTpO0mBM</a></p>
 			<h3>What is "Inspection" and "Execution" on each stage?</h3>
 			<p>Inspection is the time you only thinking what alg to execute on the current cube. It is duration to the first move that is not U face turn or the move not canceled by the following moves.</p>
 			<p>Execution time is (total time) - (inspection time).</p>
