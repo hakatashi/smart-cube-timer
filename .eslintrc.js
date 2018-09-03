fb359ae2f13fe1c763177013170bb2d6a4504c27
Koki Takahashi <hakatasiloving@gmail.com>
2018-09-03 17:12:45 +0900

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
 			},
 		}],
 		'node/no-unsupported-features': 'off',
+		'node/no-unsupported-features/es-syntax': 'off',
 		'private-props/no-use-outside': 'off',
 		'max-params': 'off',
 		'no-underscore-dangle': 'off',
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import Vuetify from 'vuetify';
 
 Vue.use(Vuetify);
