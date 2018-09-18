8c3f7f3076c4f6a6ef533fca3f9c6db12a38ab8c
Koki Takahashi <hakatasiloving@gmail.com>
2018-09-18 15:19:32 +0900

    Upgrade @hakatashi/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,29 +9,14 @@ module.exports = {
 	},
 	extends: [
 		'@hakatashi',
-		'plugin:vue/recommended',
-	],
-	// required to lint *.vue files
-	plugins: [
-		'vue',
 	],
 	// add your custom rules here
 	rules: {
-		'vue/html-indent': ['error', 'tab'],
-		'vue/mustache-interpolation-spacing': ['error', 'never'],
-		'vue/max-attributes-per-line': ['error', {
-			singleline: 3,
-			multiline: {
-				max: 1,
-				allowFirstLine: false,
-			},
-		}],
 		'node/no-unsupported-features': 'off',
 		'node/no-unsupported-features/es-syntax': 'off',
 		'private-props/no-use-outside': 'off',
 		'max-params': 'off',
 		'no-underscore-dangle': 'off',
-		'no-lonely-if': 'off',
 	},
 	globals: {},
 };
